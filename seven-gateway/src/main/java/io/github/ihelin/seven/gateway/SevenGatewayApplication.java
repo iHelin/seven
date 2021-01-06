4828bb7fb881ddf1ab0ad6c2ff9a04b0a2fa34c2
iHelin <ihelin@outlook.com>
2021-01-06 10:50:52 +0800

    update

--- seven-gateway/src/main/java/io/github/ihelin/seven/gateway/SevenGatewayApplication.java
+++ seven-gateway/src/main/java/io/github/ihelin/seven/gateway/SevenGatewayApplication.java
@@ -1,4 +1,4 @@
-package io.github.ihelin.seven.coupon;
+package io.github.ihelin.seven.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

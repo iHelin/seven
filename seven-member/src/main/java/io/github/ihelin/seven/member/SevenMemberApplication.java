49718e6e3f95dfb745a39d00455fff7fe390ed24
iHelin <ihelin@outlook.com>
2021-01-14 19:18:11 +0800

    调整配置文件

--- seven-member/src/main/java/io/github/ihelin/seven/member/SevenMemberApplication.java
+++ seven-member/src/main/java/io/github/ihelin/seven/member/SevenMemberApplication.java
@@ -1,15 +1,17 @@
 package io.github.ihelin.seven.member;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableFeignClients(basePackages = "io.github.ihelin.seven.member.feign")
 @SpringBootApplication
 @EnableTransactionManagement
+@MapperScan("io.github.ihelin.seven.member.dao")
+@EnableFeignClients(basePackages = "io.github.ihelin.seven.member.feign")
 public class SevenMemberApplication {
 
     public static void main(String[] args) {
@@ -17,7 +19,7 @@ public class SevenMemberApplication {
     }
 
     @Bean
-    public PaginationInterceptor paginationInterceptor(){
+    public PaginationInterceptor paginationInterceptor() {
         PaginationInterceptor interceptor = new PaginationInterceptor();
         interceptor.setOverflow(true);
         interceptor.setLimit(500);
